65c04b0154ee25b22d97a126cfd99ecc172bbb71
Leandrusk <Leandrosotocarrasco91@gmail.com>
2024-11-17 06:15:53 -0300

    mas diseño

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  email: string = '';
+  password: string = '';
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  async register() {
+    // Verificar si los campos están completos
+    if (this.email && this.password) {
+      // Si los campos son válidos, mostrar alerta de éxito
+      const alert = await this.alertController.create({
+        header: '¡Éxito!',
+        message: 'Te has registrado correctamente.',
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+    } else {
+      // Si los campos no están completos, mostrar un mensaje de advertencia
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Por favor, complete todos los campos.',
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+    }
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular'; // Importar NavController
 
 @Component({
   selector: 'app-tab3',
@@ -12,26 +13,23 @@ export class Tab3Page {
   showAlert: boolean = false; // Estado de la alerta
   alertMessage: string = ''; // Mensaje de la alerta
 
+  constructor(private navController: NavController) {} // Inyectar NavController
 
-
+  // Función que se ejecuta cuando se hace clic en el botón "Iniciar"
   onLogin() {
     if (this.email && this.password) {
-      // Si ambos campos están completos, mostramos la alerta
-      this.showAlert = true;
+      // Si ambos campos están completos, mostramos la alerta de éxito
+      this.alertMessage = '¡Inicio de sesión exitoso!';
+      this.showAlert = true; // Muestra la alerta
+
+      // Redirigir a la sección de catálogos (suponiendo que la página de catálogos es "Tab5Page")
+      this.navController.navigateForward('/tab4'); // Redirige a la página de catálogos
     } else {
       // Si falta algún campo, mostramos un mensaje de error
       this.alertMessage = 'Por favor, complete todos los campos.';
-      this.showAlert = true; // Mostramos la alerta con el mensaje de error
+      this.showAlert = true; // Muestra la alerta con el mensaje de error
     }
   }
-
-  
-
-  constructor() {}
-// Función que se ejecuta cuando se hace clic en el botón "Iniciar"
-showLoginAlert() {
-  this.showAlert = true; // Muestra la alerta
-}
 
   // Función que se ejecuta cuando la alerta es cerrada
   onAlertDismiss() {
